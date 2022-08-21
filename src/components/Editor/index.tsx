06dffa7290b009fd6d740887326de0238abb1df9
Dipankar Chaki <joy.dcj@gmail.com>
2022-08-21 16:18:35 +1000

    editor is added wow

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,5 +1,12 @@
+import { useState } from "react";
+import { RichTextEditor } from "@mantine/rte";
+
+const initialValue =
+	"<p>Your initial <b>html value</b> or an empty string to init editor without value</p>";
+
 const Editor: React.FC = () => {
-	return <>Editor</>;
+	const [value, onChange] = useState(initialValue);
+	return <RichTextEditor value={value} onChange={onChange} />;
 };
 
 export default Editor;
