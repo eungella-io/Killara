25c1ae0949fd37498a32e0e71257939343303ef8
Yeasir <yeasirh@gmail.com>
2022-08-28 13:12:54 +1000

    footer and menu

--- src/components/MenuBar/index.tsx
+++ src/components/MenuBar/index.tsx
@@ -1,7 +1,16 @@
 import style from "./MenuBar.module.scss";
+import WidgetIcon from "assets/widget.svg";
+import AddFile from "assets/add-file.svg";
+import AddFolder from "assets/add-folder.svg";
 
 const MenuBar: React.FC = () => {
-	return <div className={style.menu_bar}>Menu Bar</div>;
+	return (
+		<div className={style.menu_bar}>
+			<img src={WidgetIcon} alt="widget icon" />
+			<img src={AddFile} alt="add file icon" />
+			<img src={AddFolder} alt="add folder icon" />
+		</div>
+	);
 };
 
 export default MenuBar;
